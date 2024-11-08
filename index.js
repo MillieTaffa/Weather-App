166901b9c55f5d4b5efc6783b53126d2bb5285a8
MillieTaffa <161293050+MillieTaffa@users.noreply.github.com>
2024-11-08 13:44:11 +0200

    Starting With HTML
    Working on a basic HTML structure in my html file with the font family and axios link.
    On my Java file, I just added my API keys for later.
    I also added my fonts to my CSS

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
-apiKey: a1f90bc0bf23da8c35fe325tob5f8845
-apiUrl: https://api.shecodes.io/weather/v1/current?query={query}&key={key}
+//apiKey: a1f90bc0bf23da8c35fe325tob5f8845
+//apiUrl: https://api.shecodes.io/weather/v1/current?query={query}&key={key}
+
